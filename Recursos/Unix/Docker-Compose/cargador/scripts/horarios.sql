872a5db32db31c272898fe58daa763d02c4057ef
andorkan <andorkan@gmail.com>
2021-06-23 16:30:01 -0300

    Se saca ajuste de autonumeración de id en horarios, dado que no aplica más

--- Recursos/Unix/Docker-Compose/cargador/scripts/horarios.sql
+++ Recursos/Unix/Docker-Compose/cargador/scripts/horarios.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95ca8e04ca401c6a54f72682f1d28dc8ddd3a0038ba3840c0af22bdc0ad18788
-size 82938091
+oid sha256:f84d3e113bdb57370c2a98225f677fab2f02bfc8669c8e712cf2d1150a2cf122
+size 82937718
